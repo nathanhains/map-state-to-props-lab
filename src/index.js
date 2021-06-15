c645931e77783547efecfd7e916edc60bf85c38f
nathan hains <nathanhains14@yahoo.com>
2021-06-15 16:02:27 -0400

    commit tree 1159cbcef08c62f3ae8f7a328ca9497cb4aed28a

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App'
 
 import manageUsers from './reducers/manageUsers'
+import {Provider} from 'react-redux'
+import { createStore } from 'redux';
 
-
-// add imports and code
-
-
+const store = createStore(manageUsers)
 ReactDOM.render(
-  // add imports and code
+  <Provider store = {store}>
   <App />
-  // add imports and code
+  </Provider>
   ,
   document.getElementById('root')
 );
